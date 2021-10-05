ce30d5a96849efcc956ff4e6ab2a07d36fb31592
Khailas R <khailas303@gmail.com>
2021-10-05 22:11:03 +0530

    user profile form created

--- users/forms.py
+++ users/forms.py
@@ -5,6 +5,7 @@ from .models import UserProfile
 from django import forms
 
 
+
 class UserForm(UserCreationForm):   # user creation
     first_name = forms.CharField(
         max_length=40, required=True,
@@ -31,33 +32,55 @@ class UserForm(UserCreationForm):   # user creation
     password2 = forms.TextInput(
         attrs={'placeholder': 'Confirm Password', 'class': 'password'}
     )
-    
-    token = forms.CharField(
-        widget=forms.HiddenInput()
-    )
-    
+
+    token = forms.CharField(widget=forms.HiddenInput())
+
     class Meta:
         model = User
         fields = (
-            'username', 'first_name', 'last_name', 'email', 'password1', 'password2'
+            'first_name', 'last_name', 'email', 'password1', 'password2'
         )
-        
+
 
 class AuthForm(AuthenticationForm):     # user auth
-    username = forms.EmailField(
+    email = forms.EmailField(
         max_length=50, required=True,
-        widget = forms.TextInput(
+        widget=forms.TextInput(
             attrs={'placeholder': 'Email'}
         )
     )
     password = forms.CharField(
-        widget = forms.PasswordInput(
+        widget=forms.PasswordInput(
             attrs={'placeholder': 'Password', 'class': 'password'}
         )
     )
-    
+
     class Meta:
         model = User
         fields = (
-            'username', 'password',
+            'email', 'password',
+        )
+
+
+# basic model form for the user profile that extends Django user model
+class UserProfileForm(forms.ModelForm):
+    address = forms.CharField(
+        max_length=100, required=True, widget=forms.HiddenInput())  # hiddeninput will be hidden on the html file
+    town = forms.CharField(
+        max_length=100, required=True, widget=forms.HiddenInput())
+    county = forms.CharField(
+        max_length=100, required=True, widget=forms.HiddenInput())
+    post_code = forms.CharField(
+        max_length=40, required=True, widget=forms.HiddenInput())
+    country = forms.CharField(
+        max_length=100, required=True, widget=forms.HiddenInput())
+    longitude = forms.CharField(
+        max_length=40, required=True, widget=forms.HiddenInput())
+    latitude = forms.CharField(
+        max_length=50, required=True, widget=forms.HiddenInput())
+
+    class Meta:
+        model = UserProfile
+        fields = (
+            'address', 'town', 'county', 'post_code', 'country', 'longitude', 'latitude'
         )
